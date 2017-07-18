02a96b4a2ea65b6cf7d33a558f5ed24ff4637f65
Hayden Yu <>
2017-07-18 17:01:28 -0400

    book list view

---
+++ app/src/main/java/com/qualityreading/qualityreading/BookListViewAdapter.java
@@ -0,0 +1,10 @@
+package com.qualityreading.qualityreading;
+
+import android.widget.BaseAdapter;
+
+/**
+ * Created by hayden on 2017-07-18.
+ */
+
+public class BookListViewAdapter extends BaseAdapter {
+}
